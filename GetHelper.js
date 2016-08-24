0846107d02ca9fa2b6d313099ec0a897f521e0be
awds333 <123321234432345543@mail.ru>
2016-08-24 13:41:29 +0300

    getHelper поправил

--- GetHelper.js
+++ GetHelper.js
@@ -2,17 +2,17 @@
  * Created by user on 24.08.2016.
  */
 class GetHelper{
-    static getProjects(response) {
+    public static getProjects(response) {
     var jsonob = JSON.parse(response.getData());
     var jsonar = JSON.projects;
     return jsonar;
 }
-    static getImages(response) {
+    public static getImages(response) {
     var jsonob = JSON.parse(response.getData());
     var jsonar = JSON.images;
     return jsonar;
 }
-    static getComments(response) {
+    public static getComments(response) {
         var jsonob = JSON.parse(response.getData());
         var jsonar = JSON.comments;
         return jsonar;
