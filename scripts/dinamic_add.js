47f51470cdf040a25f8a640e1d0cf8616bbdf9c5
ZhilkoRoman <roman.zhilko@gmail.com>
2016-08-25 11:53:43 +0300

    типа фикс, но нифига

--- scripts/dinamic_add.js
+++ scripts/dinamic_add.js
@@ -1,9 +1,10 @@
 function add() {
+    for(var i=0;i<10;i++){
     var newrow=('<li  class="icon3">'+document.getElementById("quantity").value.toString()+'</li>');
     var div = document.createElement('li');
     div.style.marginLeft = 1.1+"%";
     div.className = "alert alert-success";
     var v = document.getElementById("quantity").value.toString();
     div.innerHTML = newrow;
-    document.getElementById("s2").appendChild(div);
+    document.getElementById("s2").appendChild(div);}
 }
