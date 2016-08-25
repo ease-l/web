bbb370d38333f1ee219a655100e6ec2bb714fd1d
ZhilkoRoman <roman.zhilko@gmail.com>
2016-08-25 11:42:05 +0300

    l

--- scripts/dinamic_add.js
+++ scripts/dinamic_add.js
@@ -1,8 +1,9 @@
 function add() {
-    var newrow=('<li class="icon3">textxtxt</li>');
+    var newrow=('<li  class="icon3">'+document.getElementById("quantity").value.toString()+'</li>');
     var div = document.createElement('li');
     div.style.marginLeft = 1.1+"%";
     div.className = "alert alert-success";
+    var v = document.getElementById("quantity").value.toString();
     div.innerHTML = newrow;
     document.getElementById("s2").appendChild(div);
 }
--- scripts/js.js
+++ scripts/js.js
@@ -2,7 +2,7 @@ var t,lit,i;
 function tree(liter) {
     lit=liter;
     i=1;
-    t = setInterval(show, 75);  //развертываем/сворачиваем с заданным интервалом
+    show();  //развертываем/сворачиваем с заданным интервалом
 }
 
 function show() {
@@ -10,6 +10,6 @@ function show() {
     if(el = document.getElementById(lit + i)) {
         el.style.display = (el.style.display == 'block')?'none':'block';
         i++;
+        show();
     }
-else clearInterval(t);
 }
