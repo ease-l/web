32316ceab39b56f7137753207f6703a8ed6bf064
MikhniukR <r.mikhniuk@gmail.com>
2016-08-25 17:15:21 +0300

    Add magic for comment in image

--- scripts/imagewatch.js
+++ scripts/imagewatch.js
@@ -9,23 +9,25 @@ var ExampleApplication = React.createClass({
         var jsonobject = response.data.value;
         document.getElementById("name").textContent = jsonobject.Name;
         document.getElementById("image").src = jsonobject.Url;
-        axios.get('http://localhost:51715/Image/' + imageId + '/comment')
-            .then(function (response) {
-                for(var i = 0; i < response.data.value.length; ++i) {
-                    var jsonobject = response.data.value[i];
-                    var div = document.createElement('li');
-                    var div2 = document.createElement('li');
-                    div.className = "alert alert-success";
-                    div2.className = "alert alert-success";
-                    div.appendChild(document.createTextNode(jsonobject.Name));
-                    div2.appendChild(document.createTextNode(jsonobject.Text));
-                    document.getElementById("comments").appendChild(div);
-                    document.getElementById("comments").appendChild(div2);
-                }
-            })
-            .catch(function (error) {
-                console.log(error.toString());
-            });
+        if(jsonobject.Comments.length > 0) {
+            axios.get('http://localhost:51715/Image/' + imageId + '/comment')
+                .then(function (response) {
+                    for (var i = 0; i < response.data.value.length; ++i) {
+                        var jsonobject = response.data.value[i];
+                        var div = document.createElement('li');
+                        var div2 = document.createElement('li');
+                        div.className = "alert alert-success";
+                        div2.className = "alert alert-success";
+                        div.appendChild(document.createTextNode(jsonobject.Name));
+                        div2.appendChild(document.createTextNode(jsonobject.Text));
+                        document.getElementById("comments").appendChild(div);
+                        document.getElementById("comments").appendChild(div2);
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error.toString());
+                });
+        }
     })
     .catch(function (error) {
         console.log(error.toString());
