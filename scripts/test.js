2930cef70906578a67a33991853c0b62951e5b8f
MikhniukR <r.mikhniuk@gmail.com>
2016-08-25 18:54:21 +0300

    Fix bag with project without child project in root

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,36 @@
-function addByName(name) {
-    var newrow=('<li  class="icon">'+name+'</li>');
-    var div = document.createElement('li');
-    div.style.marginLeft = 1.5+"%";
-    div.className = "alert alert-success";
-    var v = document.getElementById("quantity").value.toString();
-    div.innerHTML = newrow;
-    document.getElementById("spisok").appendChild(div);
+function addByName( idwr) {
+    console.log("++++++");
+    var level = 2;
+    axios.get('http://localhost:51715/Project/'+idwr)
+        .then(function (response) {
+            var jsonarray = response.data.value;
+            console.log(jsonarray);
+            var name = jsonarray.Name;
+            if(jsonarray.Images.length > 0){
+                console.log(name);
+                var newrow = ('<li  id=\'' + idwr +'\''+  'name = "'+ idwr +'"  class="icon2">'+
+                '<a onclick="tree(\''+idwr+'\')" rollapp-href="">' + name + '</li>');
+                var div = document.createElement('li');
+                div.style.marginLeft = 1.5*level + "%";
+                div.className = "alert alert-success";
+                div.innerHTML = newrow;
+                document.getElementById("spisok").appendChild(div);
+                if(jsonarray.Images.length > 0) {
+                    console.log("+" + name);
+                    AddImage(idwr, idwr, 0, level);
+                }
+            }else{
+                var newrow = ('<li dblclick="AddProjectToProject()" id=\'' + idwr +'\''+  'name = "'+ idname +'"  class="icon">' + name + '</li>');
+                var div = document.createElement('li');
+                div.style.marginLeft = 1.5*level + "%";
+                div.className = "alert alert-success";
+                div.innerHTML = newrow;
+                document.getElementById("spisok").appendChild(div);
+            }
+        })
+        .catch(function (error) {
+            console.log(error.toString());
+        });
 }
 
 function addByNameWithChild(name, id, level) {
@@ -16,7 +41,7 @@ function addByNameWithChild(name, id, level) {
             var id = jsonarray.Id;
             var projectMas = jsonarray.Projects;
 
-            var newrow=('<li  id=\'' + id +'\''+ ' name = "'+ id +'" class="icon2"><a onclick="tree(\''+id+'\')" rollapp-href="">'+name+'</li>');
+            var newrow=('<li dblclick="AddProjectToProject()" id=\'' + id +'\''+ ' name = "'+ id +'" class="icon2"><a onclick="tree(\''+id+'\')" rollapp-href="">'+name+'</li>');
             var div = document.createElement('li');
             div.style.marginLeft = 1.5*level+"%";
             div.className = "alert alert-success";
@@ -51,13 +76,13 @@ function AddImage(projectId, parId, k, level) {
     axios.get('http://localhost:51715/Project/'+projectId + '/image')
         .then(function (response) {
             var jsonarray = response.data.value;
+            console.log(jsonarray);
             for(var i = 0; i < jsonarray.length; i++){
                 var nameIm = jsonarray[i].Name;
-                console.log(nameIm);
                 var imageId = jsonarray[i].Id;
                 var url = jsonarray[i].Url;
                 var newrow = ('<a class = "url" href="http://localhost:63342/r-template/comment.html?idImage=' +imageId+  '">'
-                + '<li  id=\'' + parId+ '' + (k+i) +'\''+ ' name = "'+ imageId +'" style="display: none;" class="icon4">'
+                + '<li dblclick="AddProjectToProject()"  id=\'' + parId+ '' + (k+i) +'\''+ ' name = "'+ imageId +'" style="display: none;" class="icon4">'
                 +nameIm+'</li></a>');
                 var div = document.createElement('li');
                 div.style.marginLeft = 1.5*level + "%";
@@ -80,29 +105,35 @@ function addByNameChild(namet, idwr, nid, level) {
             var idname = jsonarray.Id;
             var projectMas = jsonarray.Projects;
             if(projectMas.length > 0){
-                var newrow = ('<li  id=\'' + idwr +'\''+ ' name = "'+ idname +'" style="display: none;" class="icon2"><a onclick="tree(\''+idwr+'\')" rollapp-href="">'+name+'</li>');
+                var newrow = ('<li dblclick="AddProjectToProject()"  id=\'' + idwr +'\''+ ' name = "'+ idname +'" style="display: none;" class="icon2">'+
+                '<a onclick="tree(\''+idwr+'\')" rollapp-href="">'+name+'</li>');
                 var div = document.createElement('li');
                 div.style.marginLeft = 1.5*level + "%";
                 div.className = "alert alert-success";
                 div.innerHTML = newrow;
                 document.getElementById(nid).appendChild(div);
+                console.log("+");
+                console.log(jsonarray);
                 if(jsonarray.Images.length > 0) {
                     AddImage(namet, idwr, projectMas.length, level);
                 }
                 level++;
                 addByNameWithChild2(name, idname, idwr, level);
             }else if(jsonarray.Images.length > 0){
-                var newrow = ('<li id=\'' + idwr +'\''+  'name = "'+ idname +'" style="display: none;" class="icon2"><a onclick="tree(\''+idwr+'\')" rollapp-href="">' + name + '</li>');
+                console.log(name);
+                var newrow = ('<li  id=\'' + idwr +'\''+  'name = "'+ idname +'" style="display: none;" class="icon2">'+
+                '<a onclick="tree(\''+idwr+'\')" rollapp-href="">' + name + '</li>');
                 var div = document.createElement('li');
                 div.style.marginLeft = 1.5*level + "%";
                 div.className = "alert alert-success";
                 div.innerHTML = newrow;
                 document.getElementById(nid).appendChild(div);
                 if(jsonarray.Images.length > 0) {
+                    console.log("+" + name);
                     AddImage(namet, idwr, projectMas.length, level);
                 }
             }else{
-                var newrow = ('<li id=\'' + idwr +'\''+  'name = "'+ idname +'" style="display: none;" class="icon">' + name + '</li>');
+                var newrow = ('<li dblclick="AddProjectToProject()" id=\'' + idwr +'\''+  'name = "'+ idname +'" style="display: none;" class="icon">' + name + '</li>');
                 var div = document.createElement('li');
                 div.style.marginLeft = 1.5*level + "%";
                 div.className = "alert alert-success";
@@ -129,7 +160,7 @@ var ExampleApplication = React.createClass({
                     if(projectMas.length > 0){
                         addByNameWithChild(name, id, level);
                     }else {
-                        addByName(name);
+                        addByName(id, id, "spisok", 2);
                     }
                 }
             })
