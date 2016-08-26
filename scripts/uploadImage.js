cb2db2eb82a3b0c663f478e16bcdb82d7e4af3f0
awds333 <123321234432345543@mail.ru>
2016-08-26 15:02:32 +0300

    cltkfk

--- scripts/uploadImage.js
+++ scripts/uploadImage.js
@@ -7,11 +7,22 @@ function upload() {
     data.append("uploadImage", document.getElementById('upload').files[0]);
     axios.put("http://localhost:51715/Download", data).then(function (response) {
         var url = response.data.value.Result;
-        alert(url);
+        uploadimageup(url);
         console.log(response.data.value);
     })
         .catch(function (error) {
             console.log(error)
         });
 
+}
+function uploadimageup(url) {
+    var data = new JSON;
+    data.Name = document.getElementById("upload").textContent;
+    data.Url = url;
+    axios.post("http://localhost:51715/Project/"+uploadImageId+"/image",data).then(function (response) {
+        console.log(response.data.value);
+    })
+        .catch(function (error) {
+            console.log(error)
+        });
 }
